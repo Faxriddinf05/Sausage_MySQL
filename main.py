85c30b6916834977e47a7f15cace4bba95d55b94
Faxriddinf05 <mffahriddin2005@gmail.com>
2025-11-12 12:00:46 +0500

    забыл писать коды в main.py, теперь загружаю

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from routers.users import user_router
+from routers.login import login_router
+from starlette.middleware.cors import CORSMiddleware
+from routers.products import product_router
+from routers.orders import order_router
+from routers.order_items import order_item_router
+
+
+app = FastAPI(docs_url="/", title="Sausage Factory API on MySQL")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+)
+
+app.include_router(order_item_router, tags=["Buyurtma elementlari"])
+app.include_router(order_router, tags=["Buyurtma"])
+app.include_router(user_router, tags=["Profil"])
+app.include_router(login_router)
+app.include_router(product_router, tags=["Mahsulotlar"])
